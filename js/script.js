7aa9e0c2d0e86eeac7601e14a82de34aef186eb6
JAViXLiN <zzshtou@gmail.com>
2023-04-09 00:48:17 +0800

    更新nav跟top鈕

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-$(function () {
-  $("#tabs").tabs();
+$(window).on("load",function(){
+  $('.phone').fadeIn(500);
 });
+
+$(function() {
+  // 點擊TOP按鈕後nav展開
+  $("#menu").click(function(){
+    $("#menu").toggleClass('expanded');
+    $("#green").toggleClass('expanded');
+    $("#cross").toggleClass('turn');
+    $(".menu_text").toggleClass('hidden');
+    $(".menu_li").toggleClass('hidden');
+  });
+
+  // 點擊TOP按鈕後滾動至頂部
+  document.querySelector(".topbtn").addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' }); 
+  });
+
+});
+
