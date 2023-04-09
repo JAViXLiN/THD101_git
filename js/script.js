7d4209d9c2a387e34e647d3a13d14679740220f6
JAViXLiN <zzshtou@gmail.com>
2023-04-09 20:01:21 +0800

    “更新首頁”

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
-$(window).on("load",function(){
-  $('.phone').fadeIn(500);
-});
-
-$(function() {
+$(function () {
   // 點擊TOP按鈕後nav展開
-  $("#menu").click(function(){
+  $("#menu").click(function () {
     $("#menu").toggleClass('expanded');
     $("#green").toggleClass('expanded');
     $("#cross").toggleClass('turn');
@@ -13,10 +9,22 @@ $(function() {
   });
 
   // 點擊TOP按鈕後滾動至頂部
-  document.querySelector(".topbtn").addEventListener('click', function(e) {
+  document.querySelector(".topbtn").addEventListener('click', function (e) {
     e.preventDefault();
-    window.scrollTo({ top: 0, behavior: 'smooth' }); 
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   });
+
+  // 服務內容tabs
+  var $li = $('ul.tab-title li');
+  $($li.eq(0).addClass('active').find('a').attr('href')).siblings('.tab-inner').hide();
+
+  $li.click(function (e) {
+    e.preventDefault();
+    $($(this).find('a').attr('href')).show().siblings('.tab-inner').hide();
+    $(this).addClass('active').siblings('.active').removeClass('active');
+  });
+
+
 
 });
 
