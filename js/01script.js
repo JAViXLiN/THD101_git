fa8ae3b65f6953cce8567798583b188afed6460b
JAViXLiN <zzshtou@gmail.com>
2023-04-10 23:52:59 +0800

    “更新至首頁RWD”

---
+++ js/01script.js
@@ -0,0 +1,19 @@
+$(function () {
+    // 點擊nav後nav展開
+    $("#menu").click(function () {
+        $("#menu").toggleClass('expanded');
+        $("#green").toggleClass('expanded');
+        $("#cross").toggleClass('turn');
+        $(".menu_text").toggleClass('hidden');
+        $(".menu_li").toggleClass('hidden');
+    });
+
+    // 點擊TOP按鈕後滾動至頂部
+    document.querySelector(".topbtn").addEventListener('click', function (e) {
+        e.preventDefault();
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+
+
+});
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(function () {
-  // 點擊TOP按鈕後nav展開
+  // 點擊nav後nav展開
   $("#menu").click(function () {
     $("#menu").toggleClass('expanded');
     $("#green").toggleClass('expanded');
@@ -24,7 +24,40 @@ $(function () {
     $(this).addClass('active').siblings('.active').removeClass('active');
   });
 
+  //過往案例Swiper
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    initialSlide: 1,
+    loop: true,
 
+    // pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
 
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+
+  });
+
+  //合作夥伴
+  $(".pners a").click(function (e) {
+    e.preventDefault();
+  });
+
+  //最新消息
+  var ps = document.querySelectorAll("p.posts"); // 選擇所有class="info"的<p>段落
+  var limit = 80; // 設置字數限制
+  for (var i = 0; i < ps.length; i++) { // 遍歷每一個段落
+    var p = ps[i]; // 獲取當前的段落
+    var text = p.textContent; // 獲取段落內的文字
+    if (text.length > limit) { // 如果文字超過限制
+      text = text.substring(0, limit) + "......"; // 截取前limit個字，並加上...
+      p.textContent = text; // 更新段落內的文字
+    }
+  }
 });
-
