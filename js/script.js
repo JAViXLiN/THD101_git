229ee4ae1ba0d41f33bfc6ac64f81bc6cd677c92
JAViXLiN <zzshtou@gmail.com>
2023-04-11 18:53:09 +0800

    base

--- js/script.js
+++ js/script.js
@@ -3,10 +3,28 @@ $(function () {
   $("#menu").click(function () {
     $("#menu").toggleClass('expanded');
     $("#green").toggleClass('expanded');
+    $(".menu_li>a").toggleClass('expanded');
     $("#cross").toggleClass('turn');
     $(".menu_text").toggleClass('hidden');
     $(".menu_li").toggleClass('hidden');
   });
+
+  // RWD nav的overlay展開
+  if (window.innerWidth < 415) {
+    $("#menu").click(function () {
+      $(".nav-overlay").toggleClass('expanded');
+    });
+    $(".nav-overlay").click(function () {
+      $("#menu").toggleClass('expanded');
+      $("#green").toggleClass('expanded');
+      $(".menu_li>a").toggleClass('expanded');
+      $("#cross").toggleClass('turn');
+      $(".menu_text").toggleClass('hidden');
+      $(".menu_li").toggleClass('hidden');
+      $(".nav-overlay").toggleClass('expanded');
+    });
+    
+  };
 
   // 點擊TOP按鈕後滾動至頂部
   document.querySelector(".topbtn").addEventListener('click', function (e) {
